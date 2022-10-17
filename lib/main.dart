6e5fbc972cf612fe7c251981d028ee4c76a430f4
Aldairtunq <201912945b@utea.edu.pe>
2022-10-17 18:25:42 -0500

    diseño1 avanze

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 228, 243, 245),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50.0,
+              backgroundColor: Colors.white,
+              backgroundImage: AssetImage("assets/images/imagen2.png"),
+            ),
+            Divider(),
+            Card(
+              margin: EdgeInsets.all(10.0),
+              child: ListTile(
+                title: Text("Email adress"),
+                subtitle: Text("Username@gmail.comm"),
+                leading: Icon(Icons.email, color: Color.fromARGB(255, 0, 0, 0)),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.all(10.0),
+              child: ListTile(
+                title: Text("Passwordd"),
+                subtitle: Text("..............."),
+                leading: Icon(Icons.password_outlined,
+                    color: Color.fromARGB(255, 17, 17, 17)),
+                trailing: Icon(Icons.remove_red_eye_sharp),
+              ),
+            ),
+            Card(
+              child: Text(
+                "Sign Up",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
